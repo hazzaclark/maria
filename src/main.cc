9b1c8555503bb733884ec16376e20eafa4080a9a
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-07-12 16:52:47 +0100

    updated demonstration
    testing the waters to see if the operations i have wrote actually work

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,28 @@
 // COPYRIGHT (C) HARRY CLARK 2025
-// LIGHTWEIGHT CODE EMITTER/GENERATOR FOR THE HTACHI SUPERH2
+// LIGHTWEIGHT CODE EMITTER/GENERATOR FOR THE HITACHI SUPERH2
 
 // NESTED INCLUDES
 
 #include <common.h>
+#include <impl/reg.hh>
 
 // SYSTEM INCLUDES
 
 #include <cstdio>
 
+using namespace maria;
+
 int main(void)
 {
     std::printf("HARRY CLARK - SH2 EMITTER\n");
+
+    // ACCESS THE VALUE FOR R0
+    maria::GP_REGISTER SH2_R0(0);
+    std::printf("R0 VALUE: %d\n", SH2_R0.GET_INDEX());
+
+    // ACCESS THE VALUE FOR PC
+    maria::PC SH2_PC;
+    std::printf("PC VALUE: %d\n", SH2_PC.GET_INDEX());
+    
     return 0;
 }
