890422076cdad312686b57be8f7aa5ff30dc3318
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-07-12 20:48:42 +0100

    one more test

--- src/main.cc
+++ src/main.cc
@@ -16,13 +16,17 @@ int main(void)
 {
     std::printf("HARRY CLARK - SH2 EMITTER\n");
 
-    // ACCESS THE VALUE FOR R0
-    maria::GP_REGISTER SH2_R0(0);
+    maria::GP_REGISTER SH2_R0{0};
     std::printf("R0 VALUE: %d\n", SH2_R0.GET_INDEX());
 
-    // ACCESS THE VALUE FOR PC
     maria::PC SH2_PC;
     std::printf("PC VALUE: %d\n", SH2_PC.GET_INDEX());
-    
+
+    maria::PC SH2_SP;
+    std::printf("SP VALUE: %d\n", SH2_SP.GET_INDEX());
+
+    maria::SR SH2_SR;
+    std::printf("SR VALUE: %d\n", SH2_SR.GET_INDEX());
+        
     return 0;
 }
