58e451d0ba97d127788542b8b17f0601713b5d89
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-07-13 21:30:03 +0100

    added the remaining

--- inc/impl/dmac.hh
+++ inc/impl/dmac.hh
@@ -13,6 +13,12 @@
 
 class SH2_DMAC
 {
+    // https://segaretro.org/images/2/2c/SH7604_Hardware_Manual.pdf#page=249
     private:
         enum class SH2_DMA_MODE { FIXED, INCREMENT, DECREMENT, RESVERSE };
+        enum class SH2_DMA_SIZE { BYTE, WORD, LONG, QUAD };
+        enum class SH2_DMA_BUS : std::uint8_t { CYCLE, INST };
+        enum class SH2_DMA_ADDR : std::uint8_t { SINGLE, DUAL };
+        enum class SH2_DMA_DETECT : std::uint8_t { EDGE, LEVEL };
+        enum class SH2_DMA_RESC : std::uint8_t { TXI, RXI, DREQ, RESERVE };
 };
