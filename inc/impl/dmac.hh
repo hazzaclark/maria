39c231e5b7a229f143272cd84bc5b43a5f18e12f
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-07-13 22:17:09 +0100

    forgot i included the common lib

--- inc/impl/dmac.hh
+++ inc/impl/dmac.hh
@@ -11,32 +11,35 @@
 #include <common.h>
 #include <reg.hh>
 
-class SH2_DMAC
+namespace maria
 {
-    // https://segaretro.org/images/2/2c/SH7604_Hardware_Manual.pdf#page=249
-    public:
-        enum class SH2_DMA_MODE { FIXED, INCREMENT, DECREMENT, RESVERSE };
-        enum class SH2_DMA_SIZE { BYTE, WORD, LONG, QUAD };
-        enum class SH2_DMA_BUS : std::uint8_t { CYCLE, INST };
-        enum class SH2_DMA_ADDR : std::uint8_t { SINGLE, DUAL };
-        enum class SH2_DMA_DETECT : std::uint8_t { EDGE, LEVEL };
-        enum class SH2_DMA_RESC : std::uint8_t { TXI, RXI, DREQ, RESERVE };
+    class SH2_DMAC
+    {
+        // https://segaretro.org/images/2/2c/SH7604_Hardware_Manual.pdf#page=249
+        public:
+            enum class SH2_DMA_MODE { FIXED, INCREMENT, DECREMENT, RESVERSE };
+            enum class SH2_DMA_SIZE { BYTE, WORD, LONG, QUAD };
+            enum class SH2_DMA_BUS : U8 { CYCLE, INST };
+            enum class SH2_DMA_ADDR : U8 { SINGLE, DUAL };
+            enum class SH2_DMA_DETECT : U8 { EDGE, LEVEL };
+            enum class SH2_DMA_RESC : U8 { TXI, RXI, DREQ, RESERVE };
 
-        SH2_DMAC() noexcept = default;
-        ~SH2_DMAC() noexcept;
+            SH2_DMAC() noexcept = default;
+            ~SH2_DMAC() noexcept;
 
-        // DEFINE THE REGISTERS FOR THE DMA CHANNEL
-        struct SH2_CHN
-        {
-            uint32_t SAR;
-            uint32_t DAR;
-            uint32_t TCR;
-            uint32_t CHCR;
+            // DEFINE THE REGISTERS FOR THE DMA CHANNEL
+            struct SH2_CHN
+            {
+                U32 SAR;
+                U32 DAR;
+                U32 TCR;
+                U32 CHCR;
+            };
+
+        private:
+            U32 XREF_COUNT : 24;           // XREF COUNT FOR TLB PAGING  
+            SH2_CHN CHANNELS[4];                // DMA CHANNELS
+            bool ENABLED() const;               // IS THE DMA ENABLED FOR THE CURRENT CHANNEL BEING USED    
+            SH2_DMA_BUS CURRENT_BUS;            // WHICH CURRENT BUS IS BEING USED (0 OR 1)
         };
-
-    private:
-        uint32_t XREF_COUNT : 24;           // XREF COUNT FOR TLB PAGING  
-        SH2_CHN CHANNELS[4];                // DMA CHANNELS
-        bool ENABLED() const;               // IS THE DMA ENABLED FOR THE CURRENT CHANNEL BEING USED    
-        SH2_DMA_BUS CURRENT_BUS;            // WHICH CURRENT BUS IS BEING USED (0 OR 1)
-};
+}
